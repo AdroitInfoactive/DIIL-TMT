dc3a8ed5b433e5598c450758f0a76a3418ac0023
Bharath <bharath.bommidi999@gmail.com>
2024-02-14 12:44:24 +0530

    index change

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -45,7 +45,7 @@
             <div class="col-lg-6 col-md-6 col-sm-6 col-12">
                 <div class="card card-statistic-1">
                     <div class="card-icon bg-warning">
-                        <i class="fas fa-cart-arrow-down"></i>
+                        <i class="fas fa-box"></i>
                     </div>
                     <div class="card-wrap">
                         <div class="card-header">
@@ -78,7 +78,7 @@
             <div class="col-lg-6 col-md-6 col-sm-6 col-12">
                 <div class="card card-statistic-1">
                     <div class="card-icon bg-warning">
-                        <i class="fas fa-cart-arrow-down"></i>
+                        <i class="fas fa-box"></i>
                     </div>
                     <div class="card-wrap">
                         <div class="card-header">
@@ -111,7 +111,7 @@
             <div class="col-lg-6 col-md-6 col-sm-6 col-12">
                 <div class="card card-statistic-1">
                     <div class="card-icon bg-warning">
-                        <i class="fas fa-cart-arrow-down"></i>
+                        <i class="fas fa-box"></i>
                     </div>
                     <div class="card-wrap">
                         <div class="card-header">
